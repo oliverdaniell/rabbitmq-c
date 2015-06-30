b623807e8dd219223581fb86305a9464c19ab5ef
Oliver Daniell <oliverdaniell@hotmail.com>
2015-06-30 16:18:21 +0100

    Revert "ci: add asan and tsan builds to travis ci."
    This reverts commit e4a928b270eb6b84dcd7ce03f97b981c417cd587.

--- travis.sh
+++ travis.sh
@@ -7,31 +7,15 @@ build_autotools() {
 }
 
 build_cmake() {
-  CFLAGS="-fsanitize=undefined"
-  mkdir $PWD/_build && cd $PWD/_build
+  mkdir $PWD/_build
+  cd $PWD/_build
   cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../_install
   cmake --build . --target install
   ctest -V .
 }
 
-build_asan() {
-  mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
-    -DCMAKE_C_FLAGS="-fsanitize=address,undefined -O1"
-  cmake --build . --target install
-  ctest -V .
-}
-
-build_tsan() {
-  mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/../_install \
-    -DCMAKE_C_FLAGS="-fsanitize=thread,undefined -O1"
-  cmake --build . --target install
-  ctest -V .
-}
-
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 {autotools|cmake|asan|tsan}"
+  echo "Usage: $0 {autotools|cmake}"
   exit 1
 fi
 
